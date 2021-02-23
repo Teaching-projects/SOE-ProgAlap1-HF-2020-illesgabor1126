1d532cccbd0e93d51cc16e45364ac142a36ea5f0
illesgabor1126 <79475991+illesgabor1126@users.noreply.github.com>
2021-02-23 22:44:07 +0100

    Házi fel. pipa

--- 200/my_time.py
+++ 200/my_time.py
@@ -1,120 +1,56 @@
 class Time:
-    """Egyszeru osztaly eltelt ido modellezesere
-    """
+    
 
     def __init__(self, seconds:int=0):
-        """Inicializalja az idot a megadott masodpercekkel
-
-        Args:
-            seconds (int): a masodpercek szama
-        """
-        pass
+        self.seconds=seconds
+        
         
     def to_seconds(self) -> int:
-        """Adja vissza egy `int`-ben, hogy masodpercben kifejezve mennyi az ido
-
-        Returns:
-            int: a tarolt ido masodpercekben
-
-        >>> Time(12).to_seconds()
-        12
-        >>> Time(345).to_seconds()
-        345
-        """
-        pass
+        return self.seconds
+        
 
     def _ss(self)->int:
-        """Visszaadja, hogy mennyit mutat a "masodpercmutato"
-
-        Returns:
-            int: egesz perceket leszamitva a masodpercek szama
-
-        >>> Time(12)._ss()
-        12
-        >>> Time(72)._ss()
-        12
-        >>> Time(1234)._ss()
-        34
-        """
-        pass
+        return self.seconds % 60
+        
     
     def _mm(self) -> int:
-        """Visszaadja, hogy mennyit mutat a "percmutato"
-
-        Returns:
-            int: egesz orakat leszamitva az egesz percek szma
-
-        >>> Time(12)._mm()
-        0
-        >>> Time(72)._mm()
-        1
-        >>> Time(1234)._mm()
-        20
-        """
-        pass
+        return self.seconds % 3600 // 60
+        
     
     def _hh(self) -> int:
-        """Visszaadja, hogy mennyit mutat az "oramutato", amely sosem nullazodik.
-
-        Returns:
-            int: az egesz letelt orak szama, 24 fole is mehet
+        return self.seconds // 3600
         
-        >>> Time(12)._hh()
-        0
-        >>> Time(72)._hh()
-        0
-        >>> Time(1234)._hh()
-        0
-        >>> Time(3600)._hh()
-        1
-        >>> Time(12345)._hh()
-        3
-        """
-        pass
     
     def pretty_format(self) -> str:
-        """Visszaadja az idot szep modon
-         - `s` vagy `ss` egy perc alatt
-         - `m:ss` vagy `mm:ss` egy perc felett es egy ora alatt
-         - `h:mm:ss` egy ora felett. (Az orak szama tetszolegesen nagy lehet)
-
-        Returns:
-            str: a szepen formazott ido
-
-        >>> Time(12).pretty_format()
-        '12'
-        >>> Time(72).pretty_format()
-        '1:12'
-        >>> Time(3600).pretty_format()
-        '1:00:00'
-        >>> Time(12345).pretty_format()
-        '3:25:45'
-        >>> Time(123456).pretty_format()
-        '34:17:36'
-        """
-        pass
-
-
-
-    def set_from_string(self, time:str) -> int:
-        """Beallitja az idot egy string alapjan, melynek a formatuma olyan mint a `pretty_format` eseteben.
-
-        Returns:
-            int: a beallitott ido masodpercekben
-
-        Args:
-            time (str): az ido szoveg formaban
+        return "'{0}:{1}:{2}'".format(self._hh(), self._mm(), self._ss())
         
-        >>> Time().set_from_string('12')
-        12
-        >>> Time().set_from_string('1:12')
-        72
-        >>> Time().set_from_string('1:00:00')
-        3600
-        >>> Time().set_from_string('3:25:45')
-        12345
-        >>> Time().set_from_string('111:01:23')
-        399683
-        """
-        pass
+
+
+
+    def set_from_string(self, s) -> int:
+        x=s.split(":",3)
+        l=len(x)
+        if l==3:
+            self.seconds = int (x[0])*3600 + int (x[1])*60 + int (x[2])
+        elif 1==2:
+            self.seconds = int (x[0])*60 + int (x[1])    
+        else: 
+            self.seconds = int (x[0])
+        return self.seconds
+
+t1 = Time(3600+1234)
+print(t1.to_seconds())
+print(t1._ss())
+print(t1._mm())
+print(t1._hh())
+
+print(t1.pretty_format())
+print(t1.set_from_string("1:2:3"))
+
+print(t1.pretty_format())
+print(t1.set_from_string("2:3"))
+
+print(t1.pretty_format())
+print(t1.set_from_string("15"))
+        
 
